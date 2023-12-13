d662fdc483d296a05cd44a3178006a8264489d7f
Roy <roy@gastom.com.tw>
2023-12-13 17:54:22 +0800

    first commit built env and add tailwindcss adjust layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="t-bg-orange-400 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <q-icon :name="fasHatWizard" class="t-w-6 t-h-8" />
+          </q-avatar>
+          Create your imagination
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+      <div class="t-flex t-justify-center t-items-center t-h-full">
+        <div class="t-text-gray-600 t-text-xl">
+          Maybe future...
+        </div>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { fasHatWizard } from '@quasar/extras/fontawesome-v6'
+
+export default {
+  setup () {
+    const rightDrawerOpen = ref(false)   
+    return {
+      fasHatWizard,
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-file color="orange" filled v-model="model" label="Label">
+      <template v-slot:prepend>
+        <q-icon name="cloud_upload" />
+      </template>
+    </q-file>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+</script>
